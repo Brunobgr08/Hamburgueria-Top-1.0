1d3eee5c22fc4120162ba0044b59de45f2786e9b
Bruno Guedes Rodrigues <brunobgr0810@gmail.com>
2021-11-03 01:56:16 -0300

    Adicionando States ao App

--- src/App.js
+++ src/App.js
@@ -1,8 +1,58 @@
 import "./styles/App.css";
+import { useState } from "react";
 import { MenuContainer } from "./components/MenuContainer";
 import { ShoppingCart } from "./components/ShoppingCart";
 
 function App() {
+  const [products, setProducts] = useState([
+    {
+      id: 1,
+      name: "Hamburguer",
+      category: "Sanduíches",
+      price: 14.0,
+      img: "https://i.ibb.co/fpVHnZL/hamburguer.png",
+    },
+    {
+      id: 2,
+      name: "X-Burguer",
+      category: "Sanduíches",
+      price: 16.0,
+      img: "https://i.ibb.co/djbw6LV/x-burgue.png",
+    },
+    {
+      id: 3,
+      name: "Big Kenzie",
+      category: "Sanduíches",
+      price: 18.0,
+      img: "https://i.ibb.co/FYBKCwn/big-kenzie.png",
+    },
+    {
+      id: 4,
+      name: "Fanta Guaraná",
+      category: "Bebidas",
+      price: 5.0,
+      img: "https://i.ibb.co/cCjqmPM/fanta-guarana.png",
+    },
+    {
+      id: 5,
+      name: "Coca",
+      category: "Bebidas",
+      price: 4.99,
+      img: "https://i.ibb.co/fxCGP7k/coca-cola.png",
+    },
+    {
+      id: 6,
+      name: "Fanta",
+      category: "Bebidas",
+      price: 4.99,
+      img: "https://i.ibb.co/QNb3DJJ/milkshake-ovomaltine.png",
+    },
+  ]);
+  const [userInput, setUserInput] = useState([]);
+  const [filteredProducts, setFilteredProducts] = useState([]);
+  const [currentSale, setCurrentSale] = useState([]);
+  const [cartTotal, setCartTotal] = useState(0);
+
   return (
     <div className="App">
       <header className="App-header"></header>
